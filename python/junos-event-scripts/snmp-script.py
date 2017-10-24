58f0169656dd7e608f8fbe1a74461ffbf5fe2de6
pklimai <pklimai@users.noreply.github.com>
2017-10-24 18:07:54 +0300

    Create snmp-script.py

---
+++ python/junos-event-scripts/snmp-script.py
@@ -0,0 +1,21 @@
+import jcs
+
+def main():
+    snmp_action = jcs.get_snmp_action()
+    snmp_oid = jcs.get_snmp_oid()
+
+    jcs.syslog("8", "snmp_action = ", snmp_action, " snmp_oid = ", snmp_oid)
+
+    if snmp_action == 'get':
+        if snmp_oid == '.1.3.6.1.4.1.2636.13.61.1.9.1.1':
+            jcs.emit_snmp_attributes(snmp_oid, "Integer32", "10")
+        elif snmp_oid == '.1.3.6.1.4.1.2636.13.61.1.9.1.1.1':
+            jcs.emit_snmp_attributes(snmp_oid, "Integer32", "211")
+    elif snmp_action == 'get-next':
+        if snmp_oid == '.1.3.6.1.4.1.2636.13.61.1.9.1.1':             
+            jcs.emit_snmp_attributes(".1.3.6.1.4.1.2636.13.61.1.9.1.1.1", "Integer32", "211")
+        elif snmp_oid == '.1.3.6.1.4.1.2636.13.61.1.9.1.1.1':
+            jcs.emit_snmp_attributes(".1.3.6.1.4.1.2636.13.61.1.9.1.1.2", "Integer32", "429")
+
+if __name__ == '__main__':
+    main()
